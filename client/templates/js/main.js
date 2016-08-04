87b572abccea49896f13193f914662000416af36
Trenton Pottruff <jayitinc@gmail.com>
2016-08-04 00:03:52 -0400

    Opens OAuth Generator In Browser

--- client/templates/js/main.js
+++ client/templates/js/main.js
@@ -3,3 +3,10 @@ Template.main.helpers({
     return (BotInfo.find().count() > 0);
   }
 });
+
+Template.main.events({
+  'click #oauth-button'(event){
+    console.log("Opening TwitchApps TMI...");
+    Electron.openExternal("http://twitchapps.com/tmi");
+  }
+});
